32502f36e925782280a6c37c20daad16d77c028f
kondo-riyo <r1y0.tenten.1126@gmail.com>
2021-11-29 12:09:26 +0900

    disableボタンのSCSS変更

--- components/atoms/button/disableButton.vue
+++ components/atoms/button/disableButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button
+    <!-- <button
       class="
         bg-gray-400
         text-gray-500
@@ -9,6 +9,10 @@
         rounded-lg
         "
         ><slot />
+    </button> -->
+    <button
+      class="disable-button"
+        ><slot />
     </button>
 </template>
 <script lang="ts">
@@ -17,3 +21,19 @@ export default Vue.extend({
   name: 'disableButton',
 })
 </script>
+<style lang="scss">
+@import "../../../style/disable-button.scss";
+// $gray-400: #e5e7eb;
+// $gray-500: #6b7280;
+// .disable-button {
+//   background-color: $gray-400;
+//   color: $gray-500;
+//   font-weight: bold;
+//   padding: 0.5rem 1.5rem;
+//   border-radius: 0.5rem;
+//   // padding-top: 0.5rem;
+//   // padding-bottom: 0.5rem;
+//   // padding-left: 1.5rem;
+//   // padding-right: 1.5rem;
+// }
+</style>
